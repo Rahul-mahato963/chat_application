5f6b7a3d1fa2879bf7e7c71a5271961e44cbfb13
Rahul-mahato963 <rahulmahaseth146@gmail.com>
2025-12-08 13:22:37 +0530

    production ready chat_application

---
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setOtherUsers } from '../redux/userSlice';
+const BASE_URL = "http://localhost:5000";
+
+const useGetOtherUsers = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchOtherUsers = async () => {
+            try {
+                axios.defaults.withCredentials = true;
+                const res = await axios.get(`${BASE_URL}/api/v1/user`);
+                // store
+                console.log("other users -> ",res);
+                dispatch(setOtherUsers(res.data));
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchOtherUsers();
+    }, [])
+
+}
+
+export default useGetOtherUsers
